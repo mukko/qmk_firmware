e5654eb97a46c4284d40f6ed1da4fb1366031c9e
mukoyama-koji <mukoyama-koji@dmm.com>
2018-07-11 11:22:00 +0900

    Fix switch kana

--- keyboards/ergo42/keymaps/mukko-macOS/keymap.c
+++ keyboards/ergo42/keymaps/mukko-macOS/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | Sft  |   Z  |   X  |   C  |   V  |   B  |  {   |   |  }   |   N  |   M  |   ,  |   .  |   /  |\/Sft |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
-   * |  =   | Alt  |  GUI |PrtSc |ESC   |Space/|Tab/  |   |Back  |Enter/| Del  | Left | Down |  Up  | Right|
+   * | 変換 | Alt  |  GUI |PrtSc |ESC   |Space/|Tab/  |   |Back  |Enter/| Del  | Left | Down |  Up  | Right|
    * |      |      |      |      |      |~SYMB |Shift |   |Space |~META |      |      |      |      |      |
    * `------------------------------------------------'   `------------------------------------------------'
    */
@@ -29,14 +29,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,    KC_W,   KC_E,    KC_R,             KC_T,          KC_LBRC,       KC_RBRC,    KC_Y,             KC_U,    KC_I,     KC_O,     KC_P,     KC_MINS, \
     KC_LCTRL, KC_A,    KC_S,   KC_D,    KC_F,             KC_G,          S(KC_9),       S(KC_0),    KC_H,             KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
     KC_LSFT,  KC_Z,    KC_X,   KC_C,    KC_V,             KC_B,          S(KC_LBRC),    S(KC_RBRC), KC_N,             KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  SFT_T(KC_BSLS), \
-    KC_EQL,   KC_LALT, KC_LGUI, KC_PSCR, KC_ESC, LT(SYMB, KC_SPC), SFT_T(KC_TAB), KC_BSPC,    LT(META, KC_ENT), KC_DELT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+    LGUI(KC_SPC),   KC_LALT, KC_LGUI, KC_PSCR, KC_ESC, LT(SYMB, KC_SPC), SFT_T(KC_TAB), KC_BSPC,    LT(META, KC_ENT), KC_DELT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
   ),
 
   /* META
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * |   `  |   1  |   2  |   3  |   4  |   5  |  [   |   |  ]   |   6  |   7  |   8  |   9  |   0  |      |
+   * |   `  |   1  |   2  |   3  |   4  |   5  |  [   |   |  ]   |   6  |   7  |   8  |   9  |   0  |  =   |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
-   * | Alt  |   1  |   2  |   3  |   4  |   5  |  (   |   |  )   | Left | Down |  Up  | Right|   0  |  =   |
+   * | Alt  |      |      |      |      |      |  (   |   |  )   | Left | Down |  Up  | Right|   0  |      |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
    * | Sft  |  F1  |  F2  |  F3  |  F4  |  F5  |  {   |   |  }   |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
    * |------+------+------+------+------+------+------|   |------+------+------+------+------+------+------|
@@ -45,15 +45,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [META] = LAYOUT( \
-    KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    KC_7,    KC_8,  KC_9,    KC_0, XXXXXXX, \
-    _______,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_0, KC_EQL, \
-    _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,  KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11, \
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    KC_7,    KC_8,  KC_9,    KC_0, KC_EQL, \
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,    KC_0, XXXXXXX, \
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,  KC_F7,   KC_F8,   KC_F9, KC_F10,  KC_F11, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET, _______, _______, _______ \
   ),
 
   /* SYMB
    * ,------------------------------------------------.   ,------------------------------------------------.
-   * | Tab  |   !  |   @  |   #  |   $  |   %  |  [   |   |  ]   |   ^  |   &  |   *  |   (  |   )  |  ~   |
+   * |  ~   |   !  |   @  |   #  |   $  |   %  |  [   |   |  ]   |   ^  |   &  |   *  |   (  |   )  |  +   |
    * |------+------+------+------+------+------+------|   |-------------+------+------+------+------+------|
    * | Alt  |      |      |      |      |      |  (   |   |  )   |      |      |      |      |      |      |
    * |------+------+------+------+------+------+------|   |------|------+------+------+------+------+------|
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `------------------------------------------------'   `------------------------------------------------'
    */
   [SYMB] = LAYOUT( \
-    _______, S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), _______, _______, S(KC_6), S(KC_7), S(KC_8),    S(KC_9),   S(KC_0), S(KC_EQL), \
+    S(KC_GRV), S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), _______, _______, S(KC_6), S(KC_7), S(KC_8),    S(KC_9),   S(KC_0), S(KC_EQL), \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
     _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,   _______,    _______ \
